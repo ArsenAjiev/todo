2f1edfa0f1925186f6c81a2817518f8d982765f1
ArsenAjiev <selftcoder@gmail.com>
2021-09-08 21:40:34 +0300

    add todo

---
+++ todo/notes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+# notes/models.py
+from django.db import models
+
+
+class Note(models.Model):
+   title = models.CharField(max_length=50)
+   text = models.TextField()
+   created_at = models.DateTimeField(
+       auto_now_add=True, db_index=True
+   )
